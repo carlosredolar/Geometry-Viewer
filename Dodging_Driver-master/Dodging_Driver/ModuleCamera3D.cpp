38e87fff09b4d73fcb3214ef58dec5b775b1ea1c
oscarroyo4 <47557315+oscarroyo4@users.noreply.github.com>
2020-10-01 18:23:02 +0200

    Removing some things

--- Dodging_Driver-master/Dodging_Driver/ModuleCamera3D.cpp
+++ Dodging_Driver-master/Dodging_Driver/ModuleCamera3D.cpp
@@ -1,9 +1,7 @@
 #include "Globals.h"
 #include "Application.h"
 #include "ModulePlayer.h"
-#include "PhysBody3D.h"
 #include "ModuleCamera3D.h"
-#include "PhysVehicle3D.h"
 
 ModuleCamera3D::ModuleCamera3D(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
@@ -101,7 +99,6 @@ update_status ModuleCamera3D::Update(float dt)
 	}
 	else {
 		mat4x4 matrix;
-		App->player->vehicle->GetTransform(&matrix);
 
 		Position = matrix.translation();
 
