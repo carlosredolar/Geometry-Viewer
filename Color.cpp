2e9e9d3723ae581b61a5da02807d12d73fea554f
Oscar Royo <47557315+oscarroyo4@users.noreply.github.com>
2020-10-02 08:17:00 +0200

    Reorganize and project fix

---
+++ Color.cpp
@@ -0,0 +1,14 @@
+
+#include "Color.h"
+
+Color Red = Color(1.0f, 0.0f, 0.0f);
+Color Green = Color(0.0f, 1.0f, 0.0f);
+Color Deep_Green = Color(0.0f, 0.5f, 0.0f);
+Color Grass = Color(0.2f, 1.0f, 0.3f);
+Color Blue = Color(0.0f, 0.0f, 1.0f);
+Color Sky = Color(0.0f, 0.7f, 1.0f);
+Color Black = Color(0.0f, 0.0f, 0.0f);
+Color White = Color(1.0f, 1.0f, 1.0f);
+Color Purple = Color(0.5f, 0.0f, 0.5f);
+Color Gray = Color(1.0f, 1.0f, 1.0f, 0.5f);
+Color Brown = Color(0.66f, 0.33f, 0.0f);
---
+++ Color.h
@@ -0,0 +1,38 @@
+
+#pragma once
+
+struct Color
+{
+	float r, g, b, a;
+	
+	Color() : r(0.0f), g(0.0f), b(0.0f), a(1.0f)
+	{}
+
+	Color(float r, float g, float b, float a = 1.0f) : r(r), g(g), b(b), a(a)
+	{}
+
+	void Set(float r, float g, float b, float a = 1.0f)
+	{
+		this->r = r;
+		this->g = g;
+		this->b = b;
+		this->a = a;
+	}
+
+	float* operator & ()
+	{
+		return (float*)this;
+	}
+};
+
+extern Color Red;
+extern Color Green;
+extern Color Deep_Green;
+extern Color Grass;
+extern Color Blue;
+extern Color Sky;
+extern Color Black;
+extern Color White;
+extern Color Purple;
+extern Color Gray;
+extern Color Brown;
