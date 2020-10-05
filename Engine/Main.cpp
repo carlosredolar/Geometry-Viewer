45306c56b5eb192e7c1b266867177af488c69c5b
oscarroyo4 <47557315+oscarroyo4@users.noreply.github.com>
2020-10-05 22:38:43 +0200

    MathGeoLib included

--- Engine/Main.cpp
+++ Engine/Main.cpp
@@ -10,6 +10,12 @@
 #pragma comment( lib, "glew/libx86/glew32.lib" )
 #pragma comment( lib, "glew/libx86/glew32s.lib" )
 
+//#ifdef _DEBUG
+//	#pragma comment( lib, "MathGeoLib/libx86/Debug/MathGeoLib.lib" )
+//#else
+//	#pragma comment( lib, "MathGeoLib/libx86/Release/MathGeoLib.lib" )
+//#endif
+
 enum main_states
 {
 	MAIN_CREATION,
