56df377bf810d7698b67e511e07d88e112dcafe0
oscarroyo4 <47557315+oscarroyo4@users.noreply.github.com>
2020-10-16 08:33:13 +0200

    cube rendering with vertices

--- Engine/ModuleScene.cpp
+++ Engine/ModuleScene.cpp
@@ -145,50 +145,63 @@ update_status ModuleScene::Update(float dt)
 	//glDrawArrays(GL_TRIANGLES, 0, shapesize/3);
 	//glDisableClientState(GL_VERTEX_ARRAY);
 
-	//int index[36] =
-	//{
-	//	0,1,2,
-	//	0,2,3,
-	//	4,0,3,
-	//	4,3,7,
-	//	1,5,6,
-	//	1,6,2,
-	//	5,4,7,
-	//	5,7,6,
-	//	3,2,6,
-	//	3,6,7,
-	//	0,5,1,
-	//	0,1,5
-	//};
-	//
-	//static const float verts[24] =
-	//{
-	//	0.f,0.f,0.f,//0
-	//	1.f,0.f,0.f,//1
-	//	1.f,1.f,0.f,//2
-	//	0.f,1.f,0.f,//3
-	//	0.f,0.f,-1.f,//4
-	//	1.f,0.f,-1.f,//5
-	//	1.f,1.f,-1.f,//6
-	//	0.f,1.f,-1.f//7
-	//};
-
-	//glEnableClientState(GL_VERTEX_ARRAY);
-
-	//uint my_id = 0;
-	//glGenBuffers(1, (GLuint*)&(my_id));
-	//glBindBuffer(GL_ARRAY_BUFFER, my_id);
-	//glBufferData(GL_ARRAY_BUFFER, sizeof(verts) * 3, verts, GL_STATIC_DRAW);
-	//
-	//uint my_indices = 0;
-	//glGenBuffers(1, (GLuint*)&(my_indices));
-	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, my_indices);
-	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index), index, GL_STATIC_DRAW);
-
-	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, my_indices);
-	//glDrawElements(GL_TRIANGLES, sizeof(index), GL_UNSIGNED_BYTE, NULL);
-
-	//glDisableClientState(GL_VERTEX_ARRAY);
+	static float vertex[] = {
+						1.0f, 1.0f, 1.0f, //v0 
+						-1.0f, 1.0f, 1.0f, //v1
+						-1.0f, -1.0f, 1.0f, //v2
+						1.0f, -1.0f, 1.0f, //v3
+						1.0f, -1.0f, -1.0f, //v4
+						1.0f, 1.0f, -1.0f, //v5
+						-1.0f, 1.0f, -1.0f, //v6
+						-1.0f, -1.0f, -1.0f //v7
+	};
+	static uint indices[] = { //Front
+							  0, 1, 2,
+							  2, 3, 0,
+							  //Right
+							  0, 3, 4,
+							  4, 5, 0,
+							  //Up
+							  0, 5, 6,
+							  6, 1, 0,
+							  //Back
+							  7, 6, 5,
+							  5, 4, 7,
+							  //Left
+							  7, 2, 1,
+							  1, 6, 7,
+							  //Back
+							  7, 4, 3,
+							  3, 2, 7
+	};
+	
+	//Vertex
+	uint vert_id = 0;
+	glGenBuffers(1, (GLuint*)& (vert_id));
+	glBindBuffer(GL_ARRAY_BUFFER, vert_id);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertex), vertex, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	
+	//Index
+	uint ind_id = 0;
+	glGenBuffers(1, (GLuint*)& (ind_id));
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ind_id);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	
+	//Draw
+	glEnableClientState(GL_VERTEX_ARRAY);
+	
+	glBindBuffer(GL_ARRAY_BUFFER, vert_id);
+	glVertexPointer(3, GL_FLOAT, 0, NULL);
+	
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ind_id);
+	glDrawElements(GL_TRIANGLES, sizeof(indices), GL_UNSIGNED_INT, NULL);
+	
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	
+	glDisableClientState(GL_VERTEX_ARRAY);
 
 	return UPDATE_CONTINUE;
 }
